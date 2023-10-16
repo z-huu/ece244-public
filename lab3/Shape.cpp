94b844615ddb55ae77f76deeb6215bc09c65377f
z-huu <zach_1003@hotmail.com>
2023-10-16 12:05:52 -0400

    adding initial lab3 release files

---
+++ lab3/Shape.cpp
@@ -0,0 +1,15 @@
+//
+//  Shape.cpp
+//  lab3
+//
+//  Modified by Tarek Abdelrahman on 2020-10-04.
+//  Created by Tarek Abdelrahman on 2018-08-25.
+//  Copyright © 2018-2020 Tarek Abdelrahman.
+//
+//  Permission is hereby granted to use this code in ECE244 at
+//  the University of Toronto. It is prohibited to distribute
+//  this code, either publicly or to third parties.
+
+//  ECE244 Student:
+//  Write the implementation (and only the implementation) of the Shape class below
+
---
+++ lab3/parser.cpp
@@ -0,0 +1,67 @@
+//
+//  parser.cpp
+//  lab3
+//
+//  Modified by Tarek Abdelrahman on 2020-10-04.
+//  Created by Tarek Abdelrahman on 2018-08-25.
+//  Copyright © 2018-2020 Tarek Abdelrahman.
+//
+//  Permission is hereby granted to use this code in ECE244 at
+//  the University of Toronto. It is prohibited to distribute
+//  this code, either publicly or to third parties.
+
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#include "globals.h"
+#include "Shape.h"
+
+// This is the shape array, to be dynamically allocated
+Shape** shapesArray;
+
+// The number of shapes in the database, to be incremented 
+int shapeCount = 0;
+
+// The value of the argument to the maxShapes command
+int max_shapes;
+
+// ECE244 Student: you may want to add the prototype of
+// helper functions you write here
+
+int main() {
+
+    string line;
+    string command;
+    
+    cout << "> ";         // Prompt for input
+    getline(cin, line);    // Get a line from standard input
+
+    while ( !cin.eof () ) {
+        // Put the line in a linestream for parsing
+        // Making a new sstream for each line so flags etc. are cleared
+        stringstream lineStream (line);
+        
+        // Read from string stream into the command
+        // The only way this can fail is if the eof is encountered
+        lineStream >> command;
+
+        // Check for the command and act accordingly
+        // ECE244 Student: Insert your code here
+        
+        
+        
+        
+        // Once the command has been processed, prompt for the
+        // next command
+        cout << "> ";          // Prompt for input
+        getline(cin, line);
+        
+    }  // End input loop until EOF.
+    
+    return 0;
+}
+
